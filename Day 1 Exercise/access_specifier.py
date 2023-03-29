25390d8e8ac69f771183e83f93673d9222d1bd39
Uchihadi <adilovescode@gmail.com>
2023-03-29 22:53:57 +0800

    update day 2 OOP

--- Day 1 Exercise/access_specifier.py
+++ Day 1 Exercise/access_specifier.py
@@ -1,14 +1,27 @@
 #lex_auth_01275045546160947226
 class Athlete:
     def __init__(self,name,gender):
-        self.name=name
-        self.gender=gender
+        self.__name = name
+        self.__gender = gender
+
+    def set_name (self, name):
+        self.__name = name
+    def get_name(self):
+        return self.__name
+    
+    def set_gender (self, gender):
+        self.__gender = gender
+    def get_gender(self):
+        return self.__gender
 
     def running(self):
-        if(self.gender=="girl"):
+        if(self.__gender=="girl"):
             print("150mtr running")
         else:
             print("200mtr running")
                                         
-athlete1 = Athlete("Maria", "girl")
-print(athlete1.running)
+Athlete("Thomas", "boy")
+Athlete.set_name("Maria")
+Athlete.set_gender("girl")
+print(Athlete.get_name())
+print(Athlete.running())
---
+++ Day 2/getter_static.py
@@ -0,0 +1,21 @@
+class Mobile:
+    __discount = 50
+    def __init__(self, price, brand):
+        self.price = price
+        self.brand = brand
+
+    def purchase(self):
+        total = self.price - self.price * Mobile.__discount / 100
+        print ("Total is ",total)
+
+    def get_discount(self):
+        return Mobile.__discount
+
+    def set_discount(self,discount):
+        Mobile.__discount = discount
+
+mob1=Mobile(20000, "Apple")
+mob2=Mobile(30000, "Apple")
+mob3=Mobile(5000, "Samsung")
+
+print(mob1.get_discount())
