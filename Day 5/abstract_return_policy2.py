6defbebf8ffeb65ef93f231a8abdfdeb9887cda6
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-31 17:17:00 +0800

    update

---
+++ Day 5/abstract_return_policy2.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+class Product(metaclass=ABCMeta):
+    @abstractmethod
+    def return_policy(self):
+        pass
+
+class Furniture (Product):
+    def return_policy(self):
+        print("Furnitures cannot be returned")
+
+class Sofa(Furniture):
+    pass
+
+Sofa()
